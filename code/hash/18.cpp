28182d3acdaf26305673969ddf22c251bec919e5
warmdoggie <wangxinyuexlin@163.com>
2026-01-14 19:56:13 +0800

    hash end

--- code/hash/18.cpp
+++ code/hash/18.cpp
@@ -11,18 +11,23 @@ public:
         sort(nums.begin(),nums.end());
         for(int a  = 0;a<nums.size();a++){
             if(nums[0]>target) return result;
-            if(a>0 && nums[a] == nums[a-1])a++;
+            if(a>0 && nums[a] == nums[a-1])continue;
             for(int b = a+1;b<nums.size();b++){
-                if( nums[b] == nums[b-1])b++;
+                // if( nums[b] == nums[b-1])
+                //     b++;
                 int c = b + 1;
                 int d = nums.size() - 1;
                 while(b < c && c < d){
-                    if(nums[a] + nums[b] + nums[c] + nums[d] > target)d--;
-                    else if(nums[a] + nums[b] + nums[c] + nums[d] < target)c++;
+                    if(nums[a] + nums[b] + nums[c] + nums[d] > target)
+                        d--;
+                    else if(nums[a] + nums[b] + nums[c] + nums[d] < target)
+                        c++;
                     else{
                         result.push_back(vector<int>{nums[a] ,nums[b] , nums[c] , nums[d]});
-                        while(b < c && c < d && nums[c] ==  nums[c + 1])c++;
-                        while(b < c && c < d && nums[d] ==  nums[d - 1])d--;
+                        while(b < c && c < d && nums[c] ==  nums[c + 1])
+                            c++;
+                        while(b < c && c < d && nums[d] ==  nums[d - 1])
+                            d--;
                         c++;
                         d--;
                     }
